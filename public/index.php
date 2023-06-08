1e39f7ab63f771b37928b90f10db922cc1f0f15d
Arturs Cerjaks <arturscerjaks@gmail.com>
2023-06-08 19:15:56 +0300

    change routing logic, outsourc code from index.php to jokes.php, add home page

--- public/index.php
+++ public/index.php
@@ -1,29 +1,11 @@
 <?php
-try {
-    $pdo = new PDO('mysql:host=mysql;dbname=ijdb;charset=utf8mb4', 'ijdbuser', 'mypassword');
 
-    $sql = 'SELECT `joketext` FROM `joke`';
-    $result = $pdo->query($sql);
+$title = 'Internet Joke Database';
 
-    while ($row = $result->fetch()) {
-        $jokes[] = $row['joketext'];
-        }
-        $title = 'Joke list';
-        // Start the buffer
-        ob_start();
-        // Include the template. The PHP code will be executed,
-        // but the resulting HTML will be stored in the buffer
-        // rather than sent to the browser.
-        include __DIR__ . '/../templates/jokes.html.php';
-        // Read the contents of the output buffer and store them
-        // in the $output variable for use in layout.html.php
-        $output = ob_get_clean();
+ob_start();
 
-} catch (PDOException $e) {
-    $title = 'An error has occurred';
+include  __DIR__ . '/../templates/home.html.php';
 
-    $output = 'Database error: ' . $e->getMessage() . ' in ' .
-  $e->getFile() . ':' . $e->getLine();
-}
+$output = ob_get_clean();
 
 include  __DIR__ . '/../templates/layout.html.php';
