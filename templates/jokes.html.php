9c5fc17d7422e7a20800230a718bfd880186c725
Arturs Cerjaks <arturscerjaks@gmail.com>
2023-06-09 20:01:32 +0300

    Change jokes.html.php template and jokes.css styling

--- templates/jokes.html.php
+++ templates/jokes.html.php
@@ -1,5 +1,5 @@
 <?php foreach ($jokes as $joke) : ?>
-    <blockquote>
+    <blockquote class="blockquote">
         <p>
             <?= htmlspecialchars($joke['joketext'], ENT_QUOTES, 'UTF-8') ?>
             (by <a href="mailto:<?php
@@ -13,10 +13,11 @@
                     ENT_QUOTES,
                     'UTF-8'
                 ); ?></a>)
+                </p>
         <form action="deletejoke.php" method="post">
             <input type="hidden" name="id" value="<?= $joke['id'] ?>">
             <input type="submit" value="Delete">
         </form>
-        </p>
+        
     </blockquote>
 <?php endforeach; ?>
