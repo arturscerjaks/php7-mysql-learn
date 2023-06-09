52c222137f07a78f22d58aa5b27e542d9f26305a
Arturs Cerjaks <arturscerjaks@gmail.com>
2023-06-09 18:58:10 +0300

    Add deletejoke.php functionality, removed unnecessary form.css import in layout.html.php templ

--- public/deletejoke.php
+++ public/deletejoke.php
@@ -1,19 +1,22 @@
 <?php
-
 try {
     $pdo = new PDO(
         'mysql:host=mysql;dbname=ijdb;charset=utf8mb4',
         'ijdbuser',
         'mypassword'
     );
-    $sql = 'SELECT `id`, `joketext` FROM `joke`';
-    $result = $pdo->query($sql);
 
-    while ($row = $result->fetch()) {
-        $jokes[] = ['id' => $row['id'], 'joketext' => $row['joketext']];
-    }
+    $sql = 'DELETE FROM `joke` WHERE `id` = :id';
+    $stmt = $pdo->prepare($sql);
+    $stmt->bindValue(':id', $_POST['id']);
+    $stmt->execute();
+
+    header('location: jokes.php');
+
 } catch (PDOException $e) {
     $title = 'An error has occurred';
-    $output = 'Database error: ' . $e->getMessage() . ' in ' .
+    $output = 'Unable to connect to the database server: ' . $e->getMessage() . ' in ' .
         $e->getFile() . ':' . $e->getLine();
 }
+
+include __DIR__ . '/../templates/layout.html.php';
