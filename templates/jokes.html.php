e08dc7139186edee31f00d6425d3a485971af645
Arturs Cerjaks <arturscerjaks@gmail.com>
2023-06-13 13:42:46 +0300

    Change jokes template to include edit link that leads to editJoke controller

--- templates/jokes.html.php
+++ templates/jokes.html.php
@@ -1,4 +1,4 @@
-<p><?=$totalJokes?> jokes have been submitted to the Internet Joke Database.</p>
+<p><?= $totalJokes ?> jokes have been submitted to the Internet Joke Database.</p>
 <?php foreach ($jokes as $joke) : ?>
     <blockquote class="blockquote">
         <p>
@@ -9,16 +9,17 @@
                                     ENT_QUOTES,
                                     'UTF-8'
                                 ); ?>"><?php
-                echo htmlspecialchars(
-                    $joke['name'],
-                    ENT_QUOTES,
-                    'UTF-8'
-                ); ?></a>)
-                </p>
+                                        echo htmlspecialchars(
+                                            $joke['name'],
+                                            ENT_QUOTES,
+                                            'UTF-8'
+                                        ); ?></a>)
+            <a href="editjoke.php?id=<?= $joke['id'] ?>">Edit</a>
+        </p>
         <form action="deletejoke.php" method="post">
             <input type="hidden" name="id" value="<?= $joke['id'] ?>">
             <input type="submit" value="Delete">
         </form>
-        
+
     </blockquote>
 <?php endforeach; ?>
