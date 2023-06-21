4da08a5f50736ab858631c184b210d244221fb2e
Arturs Cerjaks <arturscerjaks@gmail.com>
2023-06-21 13:04:27 +0300

    Create construct() for AuthorController

--- controllers/AuthorController.php
+++ controllers/AuthorController.php
@@ -2,6 +2,14 @@
 
 namespace App\Controllers;
 
+use App\Classes\DatabaseTable;
+
 class AuthorController {
-    
+
+    private $authorTable;
+
+    public function __construct(DatabaseTable $authorTable) 
+    {
+        $this->authorTable = $authorTable;
+    }
 }
