114675e96d5e4bc6c3498c7c9d5ea7eae901b100
Arturs Cerjaks <arturscerjaks@gmail.com>
2023-06-26 19:57:04 +0300

    Add error messages to template

--- templates/register.html.php
+++ templates/register.html.php
@@ -1,3 +1,21 @@
+<?php
+if (!empty($errors)) :
+?>
+    <div class="errors">
+        <p>Your account could not be created, please check the following:</p>
+        <ul>
+            <?php
+            foreach ($errors as $error) :
+            ?>
+                <li><?= $error ?></li>
+            <?php
+            endforeach; ?>
+        </ul>
+    </div>
+<?php
+endif;
+?>
+
 <form action="" method="post">
     <label for="email">Your email address</label>
     <input name="author[email]" id="email" type="text">
