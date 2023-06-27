98d0310ef6474d21e1a7b3e45d932b7948c83d60
Arturs Cerjaks <arturscerjaks@gmail.com>
2023-06-27 20:41:45 +0300

    Add checkLogin($uri): ?string to interface

--- Framework/Website.php
+++ Framework/Website.php
@@ -1,10 +1,11 @@
 <?php
 
-/**This is an interface for the EntryPoint class constructor*/
+/**This is an interface for the EntryPoint class constructor. Assumes MVC pattern*/
 
 namespace Framework;
 
 interface Website {
     public function getDefaultRoute(): string;
     public function getController(string $controllerName): ?object;
+    public function checkLogin(string $uri): ?string;
 }
