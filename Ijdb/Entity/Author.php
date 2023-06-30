c679ed3f1ddea6b72ae97b8cf3585e5cc62559e0
Arturs Cerjaks <arturscerjaks@gmail.com>
2023-06-30 15:39:56 +0300

    Add PHPDoc annotations

--- Ijdb/Entity/Author.php
+++ Ijdb/Entity/Author.php
@@ -11,6 +11,16 @@ class Author {
     public string $password;
     private DatabaseTable $jokeTable;
 
+    /**Creates instance of Author class.
+     * 
+     * 
+     * Besides jokeTable param, there are 4 public initially unset variables (with var tag)
+     * @var int $this->id
+     * @var string $this->name
+     * @var string $this->email
+     * @var string $this->password
+    */
+
     public function __construct(DatabaseTable $jokeTable)
     {
         $this->jokeTable = $jokeTable;
@@ -20,5 +30,17 @@ class Author {
 
     public function getJokes(): array {
         return $this->jokeTable->find('authorid', $this->id);
+    }
+
+    /**Adds a joke to the `joke` DB table
+     * 
+     * 
+     * Changes instance's `$this->id` to `$joke['authorid']`
+    */
+
+    public function addJoke(array $joke): void {
+        $joke['authorid'] = $this->id;
+
+        $this->jokeTable->save($joke);
     }
 }
