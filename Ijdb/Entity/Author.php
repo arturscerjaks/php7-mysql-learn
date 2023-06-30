68020deb5a9fb0811ab59b20af2f71b8efcb832b
Arturs Cerjaks <arturscerjaks@gmail.com>
2023-06-30 15:35:32 +0300

    Add $jokeTable to constructor, add getJokes() and addJoke(array $joke) methods

--- Ijdb/Entity/Author.php
+++ Ijdb/Entity/Author.php
@@ -2,11 +2,23 @@
 
 namespace Ijdb\Entity;
 
+use Framework\DatabaseTable;
+
 class Author {
     public int $id;
     public string $name;
     public string $email;
     public string $password;
+    private DatabaseTable $jokeTable;
 
-    
+    public function __construct(DatabaseTable $jokeTable)
+    {
+        $this->jokeTable = $jokeTable;
+    }
+
+    /**Returns all jokes from DB table where authorid is `$this->id`*/
+
+    public function getJokes(): array {
+        return $this->jokeTable->find('authorid', $this->id);
+    }
 }
