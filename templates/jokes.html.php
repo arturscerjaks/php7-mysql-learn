85d8ba3347b02b7add0c21419edecba6f1bc5c94
Arturs Cerjaks <arturscerjaks@gmail.com>
2023-07-03 19:27:49 +0300

    Change joke template to use Joke entity to display values

--- templates/jokes.html.php
+++ templates/jokes.html.php
@@ -2,30 +2,30 @@
 <?php foreach ($jokes as $joke) : ?>
     <blockquote class="blockquote">
         <p>
-            <?= htmlspecialchars($joke['joketext'], ENT_QUOTES, 'UTF-8') ?>
+            <?= htmlspecialchars($joke->joketext, ENT_QUOTES, 'UTF-8') ?>
             (by <a href="mailto:<?php
                                 echo htmlspecialchars(
-                                    $joke['email'],
+                                    $joke->getAuthor()->email,
                                     ENT_QUOTES,
                                     'UTF-8'
                                 ); ?>"><?php
                                         echo htmlspecialchars(
-                                            $joke['name'],
+                                            $joke->name,
                                             ENT_QUOTES,
                                             'UTF-8'
                                         ); ?></a>
             on <?php
-                $jokedate = new DateTime($joke['jokedate']);
+                $jokedate = new DateTime($joke->jokedate);
                 echo $jokedate->format('jS F Y');
                 ?>)
 
-            <?php if (empty($joke) || $userId == $joke['authorid']) : ?>
-                <a href="/joke/edit/<?= $joke['id'] ?>">Edit</a>
+            <?php if (empty($joke) || $userId == $joke->authorid) : ?>
+                <a href="/joke/edit/<?= $joke->id ?>">Edit</a>
         <form action="/joke/delete" method="post">
-            <input type="hidden" name="id" value="<?= $joke['id'] ?>">
+            <input type="hidden" name="id" value="<?= $joke->id ?>">
             <input type="submit" value="Delete">
         </form>
     <?php endif; ?>
-
+        </p>
     </blockquote>
 <?php endforeach; ?>
