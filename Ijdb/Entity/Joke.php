cdc1de37a97d50d4ec4d22ede8f1aa2a2e6d905a
Arturs Cerjaks <arturscerjaks@gmail.com>
2023-07-03 15:55:36 +0300

    Add constructor and getAuthor() method. Add PHPDoc annotations

--- Ijdb/Entity/Joke.php
+++ Ijdb/Entity/Joke.php
@@ -2,6 +2,35 @@
 
 namespace Ijdb\Entity;
 
+use Framework\DatabaseTable;
+
 class Joke {
-    
+    public int $id;
+    public int $authorid;
+    public string $jokedate;
+    public string $joketext;
+    private DatabaseTable $authorTable;
+
+    /**
+     * Creates instance of Joke class.
+     * 
+     * 
+     * Besides authorTable param, there are 4 public initially unset variables (with var tag)
+     * @var int|null $this->id
+     * @var string|null $this->authorid
+     * @var string|null $this->jokedate
+     * @var string|null $this->joketext
+    */
+
+    public function __construct($authorTable)
+    {
+        $this->authorTable = $authorTable;
+    }
+
+    /**
+     * Returns Author object where author's id is `$this->authorid`*/
+
+    public function getAuthor() {
+        return $this->authorTable->find('id', $this->authorid)[0];
+    }
 }
