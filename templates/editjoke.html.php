f623a2fe8bb5f300156e33a03c56cf7cf7ffaf69
Arturs Cerjaks <arturscerjaks@gmail.com>
2023-07-05 16:26:32 +0300

    Make category checkboxes checked in case an existing joke has them

--- templates/editjoke.html.php
+++ templates/editjoke.html.php
@@ -4,7 +4,12 @@
     <textarea id="joketext" name="joke[joketext]" rows="3" cols="40"><?= $joke->joketext ?? '' ?></textarea>
     <p>Select categories for this joke:</p>
     <?php foreach ($categories as $category) : ?>
-        <input type="checkbox" name="category[]" value="<?= $category->id ?>" /> <label><?= $category->name ?></label>
+        <?php if ($joke && $joke->hasCategory($category->id)) : ?>
+            <input type="checkbox" checked name="category[]" value="<?= $category->id ?>" />
+        <?php else : ?>
+            <input type="checkbox" name="category[]" value="<?= $category->id ?>" />
+        <?php endif; ?>
+        <label><?= $category->name ?></label>
     <?php endforeach; ?>
     <input type="submit" name="submit" value="Save">
 </form>
