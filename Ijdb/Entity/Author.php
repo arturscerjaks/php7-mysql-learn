89756bf324ef43393d105ef283e89eb75707cbf8
Arturs Cerjaks <arturscerjaks@gmail.com>
2023-07-07 20:13:19 +0300

    Declare permission constants, create empty hasPermission(int $permission) method

--- Ijdb/Entity/Author.php
+++ Ijdb/Entity/Author.php
@@ -4,7 +4,16 @@ namespace Ijdb\Entity;
 
 use Framework\DatabaseTable;
 
-class Author {
+class Author
+{
+
+    const EDIT_JOKE = 1;
+    const DELETE_JOKE = 2;
+    const LIST_CATEGORIES = 3;
+    const EDIT_CATEGORY = 4;
+    const DELETE_CATEGORY = 5;
+    const EDIT_USER_ACCESS = 6;
+
     public ?int $id;
     public ?string $name;
     public ?string $email;
@@ -20,7 +29,7 @@ class Author {
      * @var string|null $this->name
      * @var string|null $this->email
      * @var string|null $this->password
-    */
+     */
 
     public function __construct(DatabaseTable $jokeTable)
     {
@@ -30,7 +39,8 @@ class Author {
     /**
      * Returns all jokes from DB table where authorid is `$this->id`*/
 
-    public function getJokes(): object {
+    public function getJokes(): object
+    {
         return $this->jokeTable->find('authorid', $this->id);
     }
 
@@ -39,11 +49,20 @@ class Author {
      * 
      * 
      * Changes instance's `$this->id` to `$joke['authorid']`
-    */
+     */
 
-    public function addJoke(array $joke): object {
+    public function addJoke(array $joke): object
+    {
         $joke['authorid'] = $this->id;
 
         return $this->jokeTable->save($joke);
+    }
+
+    /**
+     * Checks whether an user has permission
+     */
+
+    public function hasPermission(int $permission) {
+
     }
 }
