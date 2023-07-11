9d7825ea34fcde8a2338f163bf4fcea1078a0d10
Arturs Cerjaks <arturscerjaks@gmail.com>
2023-07-11 14:46:17 +0300

    Add support for markdown in jokes

--- templates/jokes.html.php
+++ templates/jokes.html.php
@@ -11,37 +11,38 @@
         <p><?= $totalJokes ?> jokes have been submitted to the Internet Joke Database.</p>
         <?php foreach ($jokes as $joke) : ?>
             <blockquote class="blockquote">
-                <p>
-                    <?= htmlspecialchars($joke->joketext, ENT_QUOTES, 'UTF-8') ?>
-                    (by <a href="mailto:<?php
-                                        echo htmlspecialchars(
-                                            $joke->getAuthor()->email,
-                                            ENT_QUOTES,
-                                            'UTF-8'
-                                        ); ?>"><?php
-                                                echo htmlspecialchars(
-                                                    $joke->getAuthor()->name,
-                                                    ENT_QUOTES,
-                                                    'UTF-8'
-                                                ); ?></a>
-                    on <?php
-                        $jokedate = new DateTime($joke->jokedate);
-                        echo $jokedate->format('jS F Y');
-                        ?>)
+                <?= (new \Framework\Markdown($joke->joketext))->toHtml() ?>
+                (by <a href="mailto:<?php
+                                    echo htmlspecialchars(
+                                        $joke->getAuthor()->email,
+                                        ENT_QUOTES,
+                                        'UTF-8'
+                                    ); ?>"><?php
+                                            echo htmlspecialchars(
+                                                $joke->getAuthor()->name,
+                                                ENT_QUOTES,
+                                                'UTF-8'
+                                            ); ?></a>
+                on <?php
+                    $jokedate = new DateTime($joke->jokedate);
+                    echo $jokedate->format('jS F Y');
+                    ?>)
 
-                    <?php if ($user) : ?>
-                        <?php if (empty($joke) || $user->id == $joke->authorid || $user->hasPermission(\Ijdb\Entity\Author::EDIT_JOKE)
-                        ) : ?>
-                            <a href="/joke/edit/<?= $joke->id ?>">Edit</a>
-                        <?php endif; ?>
-                        <?php if ($user->id == $joke->authorid || $user->hasPermission(\Ijdb\Entity\Author::DELETE_JOKE)
-                        ) : ?>
-                <form action="/joke/delete" method="post">
-                    <input type="hidden" name="id" value="<?= $joke->id ?>">
-                    <input type="submit" value="Delete">
-                </form>
-            <?php endif; ?>
-        <?php endif; ?>
+                <?php if ($user) : ?>
+                    <?php if (
+                        empty($joke) || $user->id == $joke->authorid || $user->hasPermission(\Ijdb\Entity\Author::EDIT_JOKE)
+                    ) : ?>
+                        <a href="/joke/edit/<?= $joke->id ?>">Edit</a>
+                    <?php endif; ?>
+                    <?php if (
+                        $user->id == $joke->authorid || $user->hasPermission(\Ijdb\Entity\Author::DELETE_JOKE)
+                    ) : ?>
+                        <form action="/joke/delete" method="post">
+                            <input type="hidden" name="id" value="<?= $joke->id ?>">
+                            <input type="submit" value="Delete">
+                        </form>
+                    <?php endif; ?>
+                <?php endif; ?>
             </blockquote>
         <?php endforeach; ?>
     </div>
